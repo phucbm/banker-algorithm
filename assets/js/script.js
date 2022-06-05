fb70dba223776d369ebd2ab6d3f2085e9319bfd4
phucbm <hiphucbui@gmail.com>
2022-06-05 12:39:29 +0700

    request to allocate resource

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@ class Banker{
     constructor(input){
         this.log = {
             calc: false,
-            result: true
+            result: true,
+            process: true
         };
 
         this.allocation = input.allocation || [];
@@ -17,15 +18,61 @@ class Banker{
         this.need = this.computeNeedMatrix();
 
         this.printInput();
-        this.isSystemSafe();
     }
 
-    isSystemSafe(){
-        let work = [...this.available];
-        const finish = [];
-        work.forEach((item, index) => {
-            finish[index] = false;
+    request(p_index, p_request){
+        const available = [...this.available];
+        const p_need = this.need[p_index];
+
+        if(this.log.process) console.log(`P${p_index + 1} requests [${p_request.join(',')}]`);
+
+        // request <= need
+        if(!this.isVectorALessThanOrEqualToB(p_request, p_need)){
+            if(this.log.result) console.warn(`Request rejected! [${p_request.join(',')}] asks more than it needs [${p_need.join(',')}]`);
+            return false;
+        }
+
+        // request <= available
+        if(!this.isVectorALessThanOrEqualToB(p_request, available)){
+            if(this.log.result) console.warn('Request ON HOLD due to system is not available.');
+            return false;
+        }
+
+        // check if system is safe with this request
+        const temp_available = this.minusVector(available, p_request);
+        const temp_allocation = [...this.allocation];
+        temp_allocation[p_index] = this.sumVector(temp_allocation[p_index], p_request);
+        const temp_need = [...this.need];
+        temp_need[p_index] = this.minusVector(temp_need[p_index], p_request);
+
+        const isSystemSafe = this.isSystemSafe({
+            available: temp_available,
+            allocation: temp_allocation,
+            need: temp_need
         });
+
+        // system is not safe
+        if(!isSystemSafe){
+            if(this.log.result) console.warn('Request ON HOLD due to system unsafe.');
+            return false;
+        }
+
+        // allocate resource
+        if(this.log.result) console.log('Request is being processed...');
+        // todo: allocate resource
+    }
+
+    isSystemSafe({
+                     available = [...this.available],
+                     allocation = [...this.allocation],
+                     need = [...this.need]
+                 } = {}){
+
+        // init finish array to all false
+        const finish = [];
+        available.forEach((item, index) => finish[index] = false);
+
+        // check if all finish is true
         const isAllFinished = () => finish.filter(val => val === true).length === finish.length;
 
         let loop = 0, max = Math.pow(this.processCount * this.resourceCount, 2);
@@ -34,10 +81,10 @@ class Banker{
             loop++;
             // step 2: find process
             let check = 0;
-            for(let i = 0; i < work.length; i++){
-                if(!finish[i] && this.isVectorALessThanOrEqualToB(this.need[i], work)){
+            for(let i = 0; i < available.length; i++){
+                if(!finish[i] && this.isVectorALessThanOrEqualToB(need[i], available)){
                     // step 3: update work
-                    work = this.sumVector(work, this.allocation[i]);
+                    available = this.sumVector(available, allocation[i]);
                     finish[i] = true;
                     break; // break loop and move to step 2
                 }
@@ -58,6 +105,17 @@ class Banker{
         }
         while(!isAllFinished() && !unsafeDetected);
         if(this.log.result) console.log('Is system safe?', isAllFinished(), finish);
+
+        return isAllFinished();
+    }
+
+    minusVector(a, b){
+        const result = [];
+        for(let i = 0; i < a.length; i++){
+            result.push(a[i] - b[i]);
+        }
+        if(this.log.calc) console.log('Minus:', a.join(','), '-', b.join(','), '=', result.join(','))
+        return result;
     }
 
     sumVector(a, b){
